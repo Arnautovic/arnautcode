08ceaf9ada0eeca8dbbe25f1cc5a993a256af904
Arnautovic <dizajnerweb@gmail.com>
2025-07-14 11:15:56 +0200

    Update Nav.js

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -178,12 +178,7 @@ const Nav = () => {
     <nav className={styles.nav}>
       <Section className={styles.navSection}>
         <p className={styles.navName}>
-          <Link href="/">
-          <img
-          src="/arnautcode-logo-min.svg"
-          alt={`${title} logo`}
-          className={styles.navLogo}
-        />
+<Link href="/"><img src="/arnautcode-logo-min.svg" alt={`${title} logo`} className={styles.navLogo} />
           </Link>
         </p>
         <ul className={styles.navMenu}>
