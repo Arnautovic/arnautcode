205d8690a717f60d94bdcff5256ad2bf20af1232
Arnautovic <dizajnerweb@gmail.com>
2025-07-15 09:25:38 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home({ posts, homePage }) {
               <div dangerouslySetInnerHTML={{ __html: hero.heroText }} />
             </div>
             <div className={styles.desnastrana}>
-              {hero.heroImage?.node?.sourceUrl && (<img src={hero.heroImage.node.sourceUrl} alt="Hero" />)}
+              {hero.heroImage?.node?.sourceUrl && ( <img src={hero.heroImage.node.sourceUrl} alt="Hero" /> )}
             </div>
           </div>
         </Container>
