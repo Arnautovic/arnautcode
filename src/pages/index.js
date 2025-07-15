b615ad58bb8fd0037057e100198a8e1b25792851
Arnautovic <dizajnerweb@gmail.com>
2025-07-15 09:22:57 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,34 +21,33 @@ export default function Home({ posts, homePage }) {
   return (
     <Layout>
       <WebsiteJsonLd siteTitle={title} />
+
       <Section>
         <Container>
           <div className={styles.okvir}>
             <div className={styles.levastrana}>
-                <h1 dangerouslySetInnerHTML={{ __html: hero.heroTitle }} />
-               <div dangerouslySetInnerHTML={{ __html: hero.heroText }} />
-                </div>
-            <div className={styles.desnastrana}>
-              {hero.heroImage?.node?.sourceUrl && (<img src={hero.heroImage.node.sourceUrl} alt="Hero" />
-          )} </div>
-    </div>
-    </Container>
-    </Section>
-
-     <Section>
-        <Container>
-          <h2 className={styles.Levastrana}>Posts</h2>
-            <div className={styles.posts}>
-              {posts.slice(0, 3).map((post) => (
-                <PostCard key={post.slug} post={post} />
-              ))}
+              <h1 dangerouslySetInnerHTML={{ __html: hero.heroTitle }} />
+              <div dangerouslySetInnerHTML={{ __html: hero.heroText }} />
             </div>
+            <div className={styles.desnastrana}>
+              {hero.heroImage?.node?.sourceUrl && (
+                <img src={hero.heroImage.node.sourceUrl} alt="Hero" />
+              )}
+            </div>
+          </div>
         </Container>
       </Section>
 
-
- 
-
+      <Section>
+        <Container>
+          <h2 className={styles.Levastrana}>Posts</h2>
+          <div className={styles.posts}>
+            {posts.slice(0, 3).map((post) => (
+              <PostCard key={post.slug} post={post} />
+            ))}
+          </div>
+        </Container>
+      </Section>
     </Layout>
   );
 }
@@ -57,8 +56,10 @@ export async function getStaticProps() {
   const { posts, pagination } = await getPaginatedPosts({
     queryIncludes: 'archive',
   });
+
   const { page: homePage } = await getPageByUri('/pocetna-strana/');
   console.log('SSR homePage', homePage);
+
   return {
     props: {
       posts,
